7ad0d5b5b10184d02f9b36c8b6dc29df43dc9be2
sarry7045 <sarry7045@gmail.com>
2021-12-15 15:28:57 +0530

    Changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,111 +1,90 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import { Link } from "react-router-dom";
 
-export class Navbar extends Component {
-  static propTypes = {};
-
-  render() {
-    return (
-      <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid">
-            <Link class="navbar-brand mx-4" to="#">
-              Daily News
-            </Link>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <Link class="nav-link active mx-2" aria-current="page" to="/">
-                    Home
-                  </Link>
-                </li>
-                <li class="nav-item mx-2">
-                  <Link
-                    class="nav-link active"
-                    aria-current="page"
-                    to="/business"
-                  >
-                    Business
-                  </Link>
-                </li>
-                <li class="nav-item mx-2">
-                  <Link
-                    class="nav-link active"
-                    aria-current="page"
-                    to="/entertainment"
-                  >
-                    Entertainment
-                  </Link>
-                </li>
-                <li class="nav-item mx-2">
-                  <Link
-                    class="nav-link active"
-                    aria-current="page"
-                    to="/general"
-                  >
-                    General
-                  </Link>
-                </li>
-                <li class="nav-item mx-2">
-                  <Link
-                    class="nav-link active"
-                    aria-current="page"
-                    to="/health"
-                  >
-                    Health
-                  </Link>
-                </li>
-                <li class="nav-item mx-2">
-                  <Link
-                    class="nav-link active"
-                    aria-current="page"
-                    to="/science"
-                  >
-                    Science
-                  </Link>
-                </li>
-                <li class="nav-item mx-2">
-                  <Link
-                    class="nav-link active"
-                    aria-current="page"
-                    to="/sports"
-                  >
-                    Sports
-                  </Link>
-                </li>
-                <li class="nav-item mx-2">
-                  <Link
-                    class="nav-link active"
-                    aria-current="page"
-                    to="/technology"
-                  >
-                    Technology
-                  </Link>
-                </li>
-                {/* <li class="nav-item">
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <Link class="navbar-brand mx-4" to="#">
+            Daily News
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link class="nav-link active mx-2" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item mx-2">
+                <Link
+                  class="nav-link active"
+                  aria-current="page"
+                  to="/business"
+                >
+                  Business
+                </Link>
+              </li>
+              <li class="nav-item mx-2">
+                <Link
+                  class="nav-link active"
+                  aria-current="page"
+                  to="/entertainment"
+                >
+                  Entertainment
+                </Link>
+              </li>
+              <li class="nav-item mx-2">
+                <Link class="nav-link active" aria-current="page" to="/general">
+                  General
+                </Link>
+              </li>
+              <li class="nav-item mx-2">
+                <Link class="nav-link active" aria-current="page" to="/health">
+                  Health
+                </Link>
+              </li>
+              <li class="nav-item mx-2">
+                <Link class="nav-link active" aria-current="page" to="/science">
+                  Science
+                </Link>
+              </li>
+              <li class="nav-item mx-2">
+                <Link class="nav-link active" aria-current="page" to="/sports">
+                  Sports
+                </Link>
+              </li>
+              <li class="nav-item mx-2">
+                <Link
+                  class="nav-link active"
+                  aria-current="page"
+                  to="/technology"
+                >
+                  Technology
+                </Link>
+              </li>
+              {/* <li class="nav-item">
                   <Link class="nav-link active" aria-current="page" to="/">
                     About
                   </Link>
                 </li> */}
-              </ul>
-            </div>
+            </ul>
           </div>
-        </nav>
-      </div>
-    );
-  }
-}
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,45 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date } = this.props;
-    return (
-      <div>
-        <div className="container my-3 text-center">
-          <div className="card">
-            <img
-              src={
-                !imageUrl
-                  ? "https://images.pexels.com/photos/9256572/pexels-photo-9256572.jpeg?cs=srgb&dl=pexels-evgeniy-zabrodin-9256572.jpg&fm=jpg"
-                  : imageUrl
-              }
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <h5 class="card-title">{title}...</h5>
-              <p className="card-text">{description}...</p>
-              <p className="card-text">
-                {" "}
-                <small className="text-muted">
-                  By {!author ? "Unknown" : author} on{" "}
-                  {new Date(date).toUTCString()}
-                </small>
-              </p>
-              <a
-                rel="noreferrer"
-                href={newsUrl}
-                target="_blank"
-                className="btn btn-sm btn-primary"
-              >
-                Read More
-              </a>
-            </div>
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date } = props;
+  return (
+    <div>
+      <div className="container my-3 text-center">
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? "https://images.pexels.com/photos/9256572/pexels-photo-9256572.jpeg?cs=srgb&dl=pexels-evgeniy-zabrodin-9256572.jpg&fm=jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 class="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              {" "}
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toUTCString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
